17fc94cfbfc844cfed8f062c7279d27ff7a05b2c
Pragna <brahm.pragya@gmail.com>
2025-04-27 17:36:00 +0530

    testimonial module

---
+++ database/migrations/2025_04_27_115336_create_testimonial_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('testimonials', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('store_view_id')->constrained('store_views')->onDelete('cascade');
+            $table->string('name');
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('testimonials');
+    }
+};

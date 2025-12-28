3b3600b343c0ffe9ae5ab1598a6507896e7e2a37
Pragna <brahm.pragya@gmail.com>
2025-12-28 18:58:22 +0530

    image path in contact module

--- app/Http/Controllers/Backend/ContactController.php
+++ app/Http/Controllers/Backend/ContactController.php
@@ -23,7 +23,8 @@ class ContactController extends Controller
 
             return datatables()->of($data)
                 ->addColumn('image', function ($row) {
-                    $imagePath = $row->created_by != 1 ? 'my_contacts' : 'contacts';
+                    $imagePath = ($row->created_by == 1 || $row->created_by == null)
+                        ? 'contacts' : 'my_contacts';
                     if ($row->image) {
                         return '<img src="' . asset("storage/{$imagePath}/" . $row->image) . '" style="max-height:50px;" />';
                     }
